96c243b788ef2b423059439ba08a49d34ae11f83
mooud <mooudgamer@gmail.com>
2024-03-01 15:22:41 +0330

    fixed some bug of middlewares

--- src/middlewares/authorAndAdminCheck.ts
+++ src/middlewares/authorAndAdminCheck.ts
@@ -2,14 +2,17 @@ import jwt from "jsonwebtoken";
 import createError from "http-errors";
 import { Router } from "express";
 import { db } from "../lib/db";
+import getCookies from "../lib/getCookies";
 
 const router = Router();
 
 router.use(async (req, res, next) => {
+  let token = getCookies(req).token;
+  if (token) req.headers.authorization = `Bearer ${token}`;
   if (!req.headers.authorization) {
     return next(createError.Unauthorized("Access token is required"));
   }
-  const token = req.headers.authorization.split(" ")[1];
+  token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return next(createError.Unauthorized());
   }
@@ -20,7 +23,11 @@ router.use(async (req, res, next) => {
         id: userId as string,
       },
     });
-    if (user?.Role !== "SUPER_ADMIN" && user?.Role !== "ADMIN" && user?.Role !== "AUTHOR")
+    if (
+      user?.Role !== "SUPER_ADMIN" &&
+      user?.Role !== "ADMIN" &&
+      user?.Role !== "AUTHOR"
+    )
       next(
         createError.Forbidden(
           "Access denied. You do not have permission to perform this action"
